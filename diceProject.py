e6a3e179e8b4d55f5badb7f63014c53e67c757d0
Maddox <maddox@xoddams-MBP.attlocal.net>
2020-09-04 18:44:01 -0500

    minor optimizations

---
+++ diceProject.py
@@ -0,0 +1,21 @@
+import random
+import matplotlib
+from colored import fg, attr
+
+run = input("Do you want to roll a die? ")
+if run == "yes":
+    while run == "yes":
+    
+        num = random.randint(1,6)
+        red = random.uniform(0.0,1.0)
+        green = random.uniform(0.0,1.0)
+        blue = random.uniform(0.0,1.0)
+    
+        rgb = matplotlib.colors.to_hex([ red, green, blue, 1 ], keep_alpha=True)
+        color = fg(rgb)
+        res = attr('reset')
+        print (color + str(num) + res)
+
+        cont = input("Do you want to roll again? ")
+        if cont == "no":
+            run = "no"
