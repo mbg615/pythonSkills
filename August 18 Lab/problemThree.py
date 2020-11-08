72534f414f94aa1b0c11f1f9bf971bad9f1f537e
mgb615 <m4dpup@gmail.com>
2020-11-07 20:38:39 -0600

    Removed old labs

--- August 18 Lab/problemThree.py
+++
@@ -1,6 +0,0 @@
-name = input("What is your name? ")
-favmovie = input("What is your favorite movie? ")
-favsnack = input("What is your favorite snack? ")
-print("Hello",name,"Here is what I know about you.")
-print("Favorite movie:",favmovie)
-print("Favorite snack:",favsnack)
--- August 18 Lab/problemTwo.py
+++
@@ -1,5 +0,0 @@
-import math
-name = input("What is your name? ")
-dob = int(input("What year were you born? "))
-age = 2020 - dob
-print("Hi",name, "you are approximately",age,"years old.")
