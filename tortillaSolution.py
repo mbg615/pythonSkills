e998534adac74aeabb8146806f7c124f1858d59a
mgb615 <m4dpup@gmail.com>
2021-03-24 21:25:28 -0500

    Work on tortilla problem

--- tortillaSolution.py
+++ tortillaSolution.py
@@ -1,18 +1,33 @@
 from random import randint
 
+def reverse(lst):
+    lst.reverse
+    return lst
+
+def valFlip(lst):
+    for i in lst:
+        if lst[i] == 0:
+            lst[i] = 1
+        if lst[i] == 1:
+            lst[i] = 0
+    return lst
+
 def main():
      # <------------------ Create List ------------------>
     tortillas_orig = []
     for i in range(10): # Change the range to lengthen the list
         tortillas_orig.append(randint(0,1))
+    tortillas_mod = tortillas_orig
 
     # <------------------ Sorting Code ------------------>
-    while not all(i for i in tortillas_orig):
-        print("In loop")
+    while not all(i for i in tortillas_mod):
+        if tortillas_mod[0] == 0:
+            print("Reversing list. . .")
+            tortillas_mod = valFlip(reverse(tortillas_mod)) # Flip the values of the reversed list.
     
     # <------------------ End Sequence ------------------>
     print("All tortillas are smiley side up!\nOriginal:", tortillas_orig, "\nCorrected:", tortillas_orig)
 
     # <------------------ Call Main ------------------>
 if __name__ == "__main__":
-    main()
+    main() # Call the main function
