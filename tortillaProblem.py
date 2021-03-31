8ffa2f90a614c7706a55b484485e48267505d928
mbg615 <69773747+mbg615@users.noreply.github.com>
2021-03-31 09:21:39 -0500

    Make minor changes to functions

--- tortillaProblem.py
+++ tortillaProblem.py
@@ -1,37 +1,44 @@
 from random import randint
 
-def reverse(lst, start):
-    listVal = start
+def flipList(lst, start):
+    store = []
     reversedList = []
+    listVal = start
+    for i in range(0, start):
+        store.append(i)
     for i in range(start, len(lst)):
         reversedList.append(lst[listVal])
         listVal += 1
-    return reversedList
-
-
-def flipList(lst, start):
-    lst = reverse(lst, start)
+    reversedList = store + reversedList
+    print(reversedList)
+    '''
     for j in lst:
                 if j == 0:
-                    lst[j] = 1
+                    reversedList[j] = 1
                 if j == 1:
-                    lst[j] = 0
-    return lst
+                    reversedList[j] = 0
+    print("Returned", reversedList)
+    '''
+    return reversedList
 
 def sort(lst):
     while not all(i for i in lst):
         for i in range(len(lst)):
+            print(i)
             if lst[i] == 0:
+                print(lst)
                 lst = flipList(lst, i)
     print("Tortilla pile is sorted:", lst)
         
 
 def main():
-    tortillas = [] 
+    tortillas = [1, 0, 0, 0, 1]
+    # Define list
+    '''
     for i in range(0, 10, 1):
         rand = randint(0,1)
         tortillas.append(rand)
-    print(type(tortillas))
+        '''
     sort(tortillas)
 
 if __name__ == "__main__":
